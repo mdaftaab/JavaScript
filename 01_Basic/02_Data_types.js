cec1cd34b7eb8c65a4328de75ca62f4938210c34
Md Aftaab <45416601+mdaftaab@users.noreply.github.com>
2023-11-08 06:28:43 +0000

    data types in js

---
+++ 01_Basic/02_Data_types.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as newr version
+
+// alert(3+5)  //we are useing nodejs not browser
+
+console.log('Aftaab');
+
+let name = 'Aftaab'     
+let age = 32            
+let isLoggedIn = false
+
+
+/* -- primitive data types --*/
+
+// number = 20, 10.5
+// string = 'hello world'
+// bigint = 098765432123456
+// boolean = true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+/* -- non primitive data types -- */
+
+//  object
+console.log(typeof 'Hello') // string
+console.log(typeof null)    // object
+console.log(typeof undefined)    // undefined
