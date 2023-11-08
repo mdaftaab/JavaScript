220d91ca2f98f38b050a7a54f578797d4e7d05c2
Md Aftaab <45416601+mdaftaab@users.noreply.github.com>
2023-11-08 09:30:56 +0000

    convertionOpretion

---
+++ 01_Basic/03_convertionOpretion.js
@@ -0,0 +1,12 @@
+// let score = "33abc" //number - NaN
+// let score = NaN       //number - 0
+// let score = undefined   //number - NaN
+// let score = "sdfsdfsd"   //number - NaN
+let score = true    //number - 1
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let vlaueInNumber = Number(score);
+console.log(typeof vlaueInNumber)
+console.log(vlaueInNumber)
