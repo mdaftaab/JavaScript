fff743f2aa6fc48482de16e3135ad547118cc0ad
Md Aftaab <45416601+mdaftaab@users.noreply.github.com>
2023-11-08 06:03:44 +0000

    variables changes

--- 02_Variables/test.js
+++ 02_Variables/test.js
@@ -1,1 +1,12 @@
 const accountId = 98765432109;
+let accoutnEmail = 'abcd@gmail.com';
+var accountPassword = 'sdfhsdfkjhsdf';
+accountCity = 'delhi';  // wrong type variable
+let accountState;  //undefined
+
+// const accountId = 876543988;  // not allowed this change
+accoutnEmail = 'new@gmail.com';
+var accountPassword = 'newpasdsfsd';
+accountCity = 'new delhi';
+
+console.table([accountId,accoutnEmail,accountPassword,accountCity,accountState])
